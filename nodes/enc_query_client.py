ef61c65e4e7a01633cd233b8d07fce61052f26b5
Thomas Donnelly <donnelly.thomas21@gmail.com>
2021-02-09 14:55:50 -0500

    Implemented enc_query_node.py as a service and added client for testing. Also fixed some configuration in package.xml and CMakeLists.txt

---
+++ nodes/enc_query_client.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+import rospy
+import argparse
+from enc_query.srv import enc_query_srv
+parser = argparse.ArgumentParser()
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-p', '--path',
+                    action='store', 
+                    help= 'ENC file input')
+group.add_argument('-d','--directory',
+                    action = 'store',
+                    help = 'Directory of ENC files to survey.')
+parser.add_argument('-l','--layers',
+                    nargs='+',
+                    type = str,
+                    help = 'Layers to query. "all" for all layers. --layer boylat wrecks') 
+parser.add_argument('-long','--longitude',
+                    action='store',
+                    help = 'longitude')
+parser.add_argument('-lat','--latitude',
+                    action='store',
+                    help = 'longitude')
+
+parser.add_argument('-a','--azimuth',
+                    action='store',
+                    help = 'Degrees from due north')
+parser.add_argument('-dist','--distance',
+                    action='store',
+                    help = 'distance in meters')
+parser.add_argument('-v','--view_angle',
+                    action='store',
+                    help = 'View angle of FOV')
+
+
+
+def enc_query_client(path, directory, layers, longitude, latitude, azimuth, distance, view_angle):
+    print("initializing client...")
+    rospy.init_node("enc_query_client")
+    print("waiting for service")
+    rospy.wait_for_service('enc_query_node')
+    print("Done waiting for service...")
+    try:
+        enc_query = rospy.ServiceProxy('enc_query_node', enc_query_srv)
+        resp1 = enc_query(path, directory, layers, longitude, latitude, azimuth, distance, view_angle)
+        print("Got Response")
+        return resp1
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+
+def usage():
+    return "%s [x y]"%sys.argv[0]
+
+if __name__ == "__main__":
+    
+    args = parser.parse_args()
+    path = args.path
+    directory = args.directory
+    layers = args.layers
+    longitude = args.longitude
+    latitude = args.latitude
+    azimuth = args.azimuth
+    distance = args.distance
+    view_angle = args.view_angle
+    print("Requesting query...")
+    features = enc_query_client(path, directory, layers, longitude, latitude, azimuth, distance, view_angle)
+    print('Response Received...')
+    print(features)
+    
+    # # args = parser.parse_args()
+    # # path = args.path
+    # directory = args.directory
+    # layers = args.layers
+    # longitude = args.longitude
+    # latitude = args.latitude
+    # azimuth = args.azimuth
+    # distance = args.distance
+    # view_angle = args.view_angle
+    # print("Requesting query...")
+    # features = enc_query_client(path, directory, layers, longitude, latitude, azimuth, distance, view_angle)
+    # for feature in features:
+    #     print(feature.name, feature.fid)
--- nodes/enc_query_node.py
+++ nodes/enc_query_node.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import rospy
+from enc_query.srv import enc_query_srv, enc_query_srvResponse
+from enc_query.msg import enc_feature_msg
 import math
 import os
 from osgeo import ogr
@@ -5,8 +10,9 @@ import argparse
 import fnmatch
 
 import project11
-parser = argparse.ArgumentParser()
 
+#parse manager
+parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-f', '--file',
                     action='store', 
@@ -32,42 +38,62 @@ parser.add_argument('-m','--meters',
                     action='store',
                     help = 'distance in meters')
 
+class enc_feature:
+    def __init__(self,fid, name = 'NoName'):
+        self.fid = fid
+        self.name = name
+
 class enc_query:
-    """ Playing with ENC files """
+    """ enc_query class  """
     def __init__(self, ENC_filename):
+        """ Initialize an enc_query
+        Args:
+            ENC_filename (string): path pointing to S-57 file to be queried
+        """
         self.ENC_filename = ENC_filename
         self.ds = ogr.Open(self.ENC_filename)
-        self.layers = ['all']
+        self.layers = []
         self.verbose = 0
         self.longlat = (0,0)#(-70.855229, 43.122453)
         self.azimuth = 0
         self.distance = 1000
         self.view_angle = 40
         self.fov = None
-
-    def getBouyLat(self):
+    
+    # Function used for testing
+    # def getBouyLat(self):
        
-        layer = self.ds.GetLayer("BOYLAT")
-        coords = []
-        for i in range(layer.GetFeatureCount()):
-            feat = layer.GetNextFeature()
-            geom = feat.GetGeometryRef()
-            coords.append((feat.GetFieldAsString('OBJNAM'), (geom.GetPoint()[1], geom.GetPoint()[0])))
-            print((feat.GetFieldAsString('OBJNAM'), (geom.GetPoint()[1], geom.GetPoint()[0])))
-        return coords
+    #     layer = self.ds.GetLayer("BOYLAT")
+    #     coords = []
+    #     for i in range(layer.GetFeatureCount()):
+    #         feat = layer.GetNextFeature()
+    #         geom = feat.GetGeometryRef()
+    #         coords.append((feat.GetFieldAsString('OBJNAM'), (geom.GetPoint()[1], geom.GetPoint()[0])))
+    #         print((feat.GetFieldAsString('OBJNAM'), (geom.GetPoint()[1], geom.GetPoint()[0])))
+    #     return coords
 
     def tempFOV(self):
+        """temporary field of view for testing
+
+        Returns:
+            pgr.Geometry : fov with Hen island and Eight-Foot rock bouys
+        """
         boxCoords = ogr.Geometry(ogr.wkbLinearRing)
         boxCoords.AddPoint(-70.863,43.123)
         boxCoords.AddPoint(-70.855,43.123)
         boxCoords.AddPoint(-70.855,43.12)
         boxCoords.AddPoint(-70.863,43.12)
         boxCoords.AddPoint(-70.863,43.123)
-
         box = ogr.Geometry(ogr.wkbPolygon)
         box.AddGeometry(boxCoords)
         return box
+
     def calculateFOV(self):
+        """calculate the field of view for the class attributes
+
+        Returns:
+            ogr.Geometry : field of view
+        """
     #          .
     #    .     |     .
     #     \    |10m /
@@ -100,96 +126,135 @@ class enc_query:
         fov.AddGeometry(fovCoords)
 
         return fov
+
     def verbosePrint(self, message, count=1):
+        """print function for when -v flag is used
+
+        Args:
+            message ( string ): message to be printed 
+            count (int, optional): Level of verbosity. Defaults to 1.
+        """
         if self.verbose>=count:
             print(message)
+
     def queryFeatures(self,layer):
+        """Querys through specified layer
+
+        Args:
+            layer (org.Layer): Layer to be queried
+
+        Returns:
+            ogr.Feature: List of features within fov 
+        """
         self.verbosePrint("Querying features...")
+
         desc = layer.GetDescription()
         numFeats = layer.GetFeatureCount()
+        featureList = []
+        
         self.verbosePrint('Layer: ' + desc)
         self.verbosePrint('\tNumFeats: ' + str(numFeats))
-        feat = layer.GetNextFeature()
+        print("NUMFEATS: ", numFeats)
         for i in range(numFeats):
+            feat = layer.GetNextFeature()
             featGeomRef = feat.GetGeometryRef()
+            fid = feat.GetFID()
             
+            #Check if feature contains Geometry reference
             if featGeomRef is not None:
+                #Check if Geometry reference is contained within FOV
                 if self.fov.Contains(featGeomRef):
                     if feat.GetFieldIndex('OBJNAM') is not None:
                         featDesc = feat.GetFieldAsString('OBJNAM')
                     else:
                         featDesc = "NO OBJNAM"
-                    fid = feat.GetFID()
-                    self.verbosePrint("Feature IS in range of FOV")
+
+                    self.verbosePrint("Feature IS in range of FOV",2)
                     self.verbosePrint("\tFeature: " + str(featDesc))
                     self.verbosePrint("\t\tFID: " + str(fid))
+
+                    featureList.append(enc_feature_msg(featDesc,fid))
                 else:
-                    self.verbosePrint("Feature NOT within FOV")
+                    self.verbosePrint("Feature NOT within FOV",2)
             else:
                 self.verbosePrint("Feature has no Geom Ref: " + desc + ", " + str(i))
-            feat = layer.GetNextFeature()
+            
+            # feat = layer.GetNextFeature()
 
         self.verbosePrint("---------------------------")
-    def queryLayers(self):
-        self.verbosePrint("Querying layers...")
-        numLayers = self.ds.GetLayerCount()
-        # boxCoords = ogr.Geometry(ogr.wkbLinearRing)
-        # boxCoords.AddPoint(-70.863,43.123)
-        # boxCoords.AddPoint(-70.855,43.123)
-        # boxCoords.AddPoint(-70.855,43.12)
-        # boxCoords.AddPoint(-70.863,43.12)
-        # boxCoords.AddPoint(-70.863,43.123)
 
-        # box = ogr.Geometry(ogr.wkbPolygon)
-        # box.AddGeometry(boxCoords)
-       
+        return featureList
+    def queryLayers(self):
+        """Query through given layers
+
+        Returns:
+            enc_feature[]: list of enc_feature type msg
+        """
+        self.verbosePrint("Querying layers...")
+
+        numLayers = self.ds.GetLayerCount()
+        featureList = []
+
         if 'all' in self.layers:
             self.verbosePrint("numLayers: " + str(numLayers))
             for i in range(numLayers):
                 layer = self.ds.GetLayerByIndex(i)
-                # try:
-                self.queryFeatures(layer)
-                # except:
-                    # print("ERROR on layer %d" % i)
+                featureList += self.queryFeatures(layer)
         else:
             for layerName in self.layers:
                 try:
                     layer = self.ds.GetLayer(layerName)
-                    self.queryFeatures(layer)
+                    featureList += self.queryFeatures(layer)
                 except:
-                    self.verbosePrint("Layer not fount: " + layerName )
-                
-            # print("Layer: %s, NumFeats: %d" % (self.layerName, self.ds.GetLayer(self.layerName).GetFeatureCount()))
-        # # if(layerName)
-        # 
+                    self.verbosePrint("Layer not found: " + layerName )
+        print("FEATURELIST:",featureList)
+        return featureList
+
             
     def run(self):
+        """Check initization of FOV
+
+        Returns:
+            enc_feature[]: list of features within query fov
+        """
         self.verbosePrint("Running...")
         fov = self.tempFOV()
         self.fov = fov
         if self.fov is not None:
-            self.queryLayers()
+            try:
+                response = self.queryLayers()
+                print("RESPONSE:", response)
+                return response
+            except:
+                self.verbosePrint("ERROR IN queryLayers ")
         else:
             self.verbosePrint('FOV is none')
 
-
-
-if __name__ == "__main__":
-    enc_files = ['/home/thomas/Downloads/ENC_ROOT/US5NH01M/TestFile/US5NH01M.000']
+def enc_query_server():
+    print("enc_query_server called...")
+    rospy.init_node('enc_query_node')
+    print("ENC NODE INITED")
+    s = rospy.Service('enc_query_node', enc_query_srv, service_handler)
+    rospy.spin()
     
-    args = parser.parse_args()
-    verbose = args.verbose
+def service_handler(req):
+    enc_files = ['/home/thomas/Downloads/ENC_ROOT/US5NH01M/TestFile/US5NH01M.000']
+    verbose = 1
+    featureList = []
+    if verbose:
+        print("Path", req.path)
+        print("Directory", req.directory)
+        print("Layers", req.layers)
+        print("Longitude", req.longitude)
+        print("Latitude", req.latitude)
+        print("Azimuth", req.azimuth)
+        print("Distance", req.distance)
+        print("View_angle", req.view_angle)
 
-    if verbose >= 2:
-        print("Arguments:")
-        arguments = vars(args)
-        for key, value in arguments.iteritems():
-            print("\t%s:\t\t%s" % (key,str(value)))
-
-    if args.file:
-        enc_files.append(args.file)
-    if args.directory:
-        directory = args.directory
+    if req.path:
+        enc_files.append(req.path)
+    if req.directory:
+        directory = req.directory
         for root, dirnames, filesnames in os.walk(directory):
             for filename in fnmatch.filter(filesnames, 'US*.000'):
                 enc_files.append(os.path.join(root,filename))
@@ -200,10 +265,47 @@ if __name__ == "__main__":
             print('Input file: %s' % enc)
         obj = enc_query(ENC_filename = enc)
         obj.verbose = verbose 
-        if args.layers:
-            obj.layers = args.layers
-        if args.position:
-            obj.longlat = tuple(args.position)
-        obj.azimuth = args.azimuth
-        obj.distance = args.meters
-        obj.run()        
+        if req.layers:
+            obj.layers = req.layers
+        if req.latitude and req.longitude:
+            obj.longlat = (req.longitude, req.latitude)
+        obj.azimuth = req.azimuth
+        obj.distance = req.distance
+        featureList += obj.run()
+    print("FINAL FEATURELIST:",featureList)
+    return enc_query_srvResponse(featureList) 
+
+if __name__ == "__main__":
+    enc_query_server()
+    # enc_files = ['/home/thomas/Downloads/ENC_ROOT/US5NH01M/TestFile/US5NH01M.000']
+    
+    # args = parser.parse_args()
+    # verbose = args.verbose
+
+    # if verbose >= 2:
+    #     print("Arguments:")
+    #     arguments = vars(args)
+    #     for key, value in arguments.iteritems():
+    #         print("\t%s:\t\t%s" % (key,str(value)))
+
+    # if args.file:
+    #     enc_files.append(args.file)
+    # if args.directory:
+    #     directory = args.directory
+    #     for root, dirnames, filesnames in os.walk(directory):
+    #         for filename in fnmatch.filter(filesnames, 'US*.000'):
+    #             enc_files.append(os.path.join(root,filename))
+
+    # for enc in enc_files:
+    #     if verbose >= 2:
+    #         print('Processing %s ' % enc)
+    #         print('Input file: %s' % enc)
+    #     obj = enc_query(ENC_filename = enc)
+    #     obj.verbose = verbose 
+    #     if args.layers:
+    #         obj.layers = args.layers
+    #     if args.position:
+    #         obj.longlat = tuple(args.position)
+    #     obj.azimuth = args.azimuth
+    #     obj.distance = args.meters
+    #     obj.run()        
